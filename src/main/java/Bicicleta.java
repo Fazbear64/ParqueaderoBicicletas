d52ee0060211d5b4eb0e72074c291e4288116331
Fazbear64 <135194826+Fazbear64@users.noreply.github.com>
2026-09-13 19:24:59 -0500

    Adicción objeto
    Bicicleta

---
+++ src/main/java/Bicicleta.java
@@ -0,0 +1,11 @@
+public class Bicicleta {
+    private int serial;
+    private int id;
+    private String color;
+
+    Bicicleta(int serial, int id, String color){
+        this.serial = serial;
+        this.id = id;
+        this.color = color;
+    }
+}
